6a7230f3d9fb8b9356c9de008296ba4650f31cbc
CesarGustavoFlores <gflowsoftware@gmail.com>
2022-10-05 12:24:49 -0300

    cositas

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,27 @@
-serie_hasta=20
+serie_hasta=25
 fibo=[1]
 for i  in range( serie_hasta):
     fibo.append(fibo[i-1]+fibo[i])
     print(fibo)
     
+adicionar= lambda a,b:a+b
+
+print(adicionar(5,8))
+
+
+# packing/unpacking
+def sumar(a,b,c,d):
+    print(a+b+c+d)
+    
+n=[1,2,3,4]
+# como pasar numeros a sumar
+# 1. no hacer, es muy malo
+print(sumar(n[1],n[2],n[3],n[4]))
+# 2- la mejor forma es desempacando
+a,b,c,d=n  # hacemos unpacking
+# lo anterior se pude hacer:
+a,b,c,d = n
+print(sumar(a,b,c,d))
+# agrupamiento
+n=[1,2,3,4]
+print(sumar(*n))
---
+++ Uno_Operador_Mat.py
@@ -0,0 +1,4 @@
+print(8-6/2+1*2)
+print(max("bcd","aeio","pq"))
+print(list(max("bcd","aeio","pq")))
+print(len(max("bcd","aeio","pq")))
